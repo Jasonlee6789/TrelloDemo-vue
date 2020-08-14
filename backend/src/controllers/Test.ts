689cfd56ec7c8eaa615c3d021103c2f3ea6c8aa2
Jasonlee6789 <jeffersonlee@163.com>
2020-08-14 19:12:49 +0800

    I use the  @Params()  to get the id(dynamic data in the address bar).

--- backend/src/controllers/Test.ts
+++ backend/src/controllers/Test.ts
@@ -1,9 +1,14 @@
-import { Controller, Get } from "koa-ts-controllers";
+import { Controller, Get, Params } from "koa-ts-controllers";
 
 @Controller("/test")
 class TestController {
   @Get("/hello")
   async hello() {
     return "Hello Test!";
+  }
+
+  @Get("/user/:id")
+  async getUser(@Params() p: { id: number }) {
+    return "当前params中的用户id是： " + p.id;
   }
 }
